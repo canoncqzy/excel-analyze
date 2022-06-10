559535f2920e9735aecac9008dcbf86960081da9
zeng_yuan <canoncqzy@163.com>
2022-06-11 00:59:47 +0800

    feat: 注释

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 // import 'ant-design-vue/dist/antd.css'
-import { Upload, message } from 'ant-design-vue'
+import { Upload } from 'ant-design-vue'
 
 const app = createApp(App)
 
 app.use(Upload).use(store).use(router).mount('#app')
-app.config.globalProperties.$message = message
+// app.config.globalProperties.$message = message
