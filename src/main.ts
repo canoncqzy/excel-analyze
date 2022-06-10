dda71ec5bc2514ff7e0ab9bd40270a0e33277bca
zeng_yuan <canoncqzy@163.com>
2022-06-10 23:51:07 +0800

    feat：添加按需引入

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'ant-design-vue/dist/antd.css'
+// import 'ant-design-vue/dist/antd.css'
 import { Upload, message } from 'ant-design-vue'
 
 const app = createApp(App)
